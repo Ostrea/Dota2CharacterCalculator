c33fcd64acb2ca5181fd05604b9ced552924a4f4
Ostrea <Ostrea@users.noreply.github.com>
2016-12-04 15:25:15 +0300

    Create db model and db context

---
+++ Dota2CharacterCalculator/Models/Hero.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Dota2CharacterCalculator.Models
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class HeroContext : DbContext
+    {
+        public DbSet<Hero> Heroes { get; set; }
+    }
+}
