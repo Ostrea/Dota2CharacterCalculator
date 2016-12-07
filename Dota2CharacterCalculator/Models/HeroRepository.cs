2c95b0badfa63c81619e79d0ad5a9dff8996da1e
Ostrea <Ostrea@users.noreply.github.com>
2016-12-07 13:52:16 +0300

    Add repository to save heroes

---
+++ Dota2CharacterCalculator/Models/HeroRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Dota2CharacterCalculator.Models
+{
+    public class HeroRepository
+    {
+        public void Save(IEnumerable<Hero> heroes)
+        {
+            using (var heroContext = new HeroContext())
+            {
+                foreach (var hero in heroes)
+                {
+                    heroContext.Heroes.Add(hero);
+                }
+                heroContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<ViewModels.Hero> GetHeroes()
+        {
+//            TODO convert to view model.
+            return null;
+        }
+
+        public void DeleteAllHeroes()
+        {
+            using (var heroContext = new HeroContext())
+            {
+                heroContext.Heroes.RemoveRange(heroContext.Heroes);
+                heroContext.SaveChanges();
+            }
+        }
+    }
+
+    public class HeroContext : DbContext
+    {
+        public DbSet<Hero> Heroes { get; set; }
+    }
+}
