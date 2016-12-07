74ddf38a2780b9920e925755fc07e2750f5d683a
Ostrea <Ostrea@users.noreply.github.com>
2016-12-07 13:50:41 +0300

    Add ef model for hero

--- Dota2CharacterCalculator/Models/Hero.cs
+++ Dota2CharacterCalculator/Models/Hero.cs
@@ -1,15 +1,28 @@
-﻿using System.Data.Entity;
-
-namespace Dota2CharacterCalculator.Models
+﻿namespace Dota2CharacterCalculator.Models
 {
     public class Hero
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-    }
 
-    public class HeroContext : DbContext
-    {
-        public DbSet<Hero> Heroes { get; set; }
+        public byte[] Icon { get; set; }
+        public string Name { get; set; }
+
+        public string PrimaryAttribute { get; set; }
+
+        public int BaseStrength { get; set; }
+        public double StrengthGrowth { get; set; }
+
+        public int BaseAgility { get; set; }
+        public double AgilityGrowth { get; set; }
+
+        public int BaseIntelligence { get; set; }
+        public double IntelligenceGrowth { get; set; }
+
+        public int BaseMovementSpeed { get; set; }
+
+        public double BaseArmor { get; set; }
+
+        public int BaseMinAttackDamage { get; set; }
+        public int BaseMaxAttackDamage { get; set; }
     }
 }
